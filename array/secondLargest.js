0ed0f1fb59c54a1afed6719828dedfd5c6850c50
Bipin MV <bipin.mv@prevalent.ai>
2023-06-17 12:16:20 +0530

    second largest of an array

---
+++ array/secondLargest.js
@@ -0,0 +1,21 @@
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+
+const findSecondLargest = (arr) => {
+  let largest = Number.NEGATIVE_INFINITY,
+    secondLargest = Number.NEGATIVE_INFINITY; // least value which JS can hold
+
+  for (let num of arr) {
+    if (num > largest) {
+      secondLargest = largest;
+      largest = num;
+    } else if (num > secondLargest && num !== largest) {
+      secondLargest = num;
+    }
+  }
+
+  return secondLargest;
+}
+
+const arr = [1, 10, 5, 35, 34, 36, 34];
+console.log(findSecondLargest(arr))
